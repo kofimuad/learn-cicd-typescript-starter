6180843f5c65cfcc787747a77f541cd7d0b3a3cd
kofimuad <iamkofiagyei@gmail.com>
2026-05-13 18:56:07 +0000

    testing ci for broken code

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,7 +4,7 @@ export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
   if (!authHeader) {
     return null;
-  }
+  
 
   const splitAuth = authHeader.split(" ");
   if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
